a2f0dd425e9d7464db1009dab026d9ea112e221d
Gustavo Carvalho <79180740+gustavocarvalho1997@users.noreply.github.com>
2022-07-26 17:39:58 -0300

    IMC v1.0

---
+++ js/script.js
@@ -0,0 +1,43 @@
+function calcularImc() {
+  //declaração de variáveis
+  let altura = document.getElementById('altura').value;
+  let peso = document.getElementById('peso').value;
+  let result = document.getElementById('result');
+  let imc = document.getElementById('imc');
+
+  // substitui vírgula por ponto
+  altura = altura.replace(',','.');
+  peso = peso.replace(',','.');
+
+  //calcular imc
+  let calculo = peso / (altura * altura);
+
+  //arredondando valor
+  calculo = calculo.toFixed(2);
+
+  // mostra na tela o imc do usuário
+  result.innerHTML = calculo + '<br/>';
+
+  // mostra se o usuário está dentro do peso ideal ou não
+  if (calculo < 18.5)
+    imc.innerHTML = '👀 Você está abaixo do peso.';
+  else if (calculo >= 18.5 && calculo < 25)
+    imc.innerHTML = '😊 Você está no peso ideal.';
+  else if (calculo >= 25 && calculo < 30)
+    imc.innerHTML = '😣 Você está acima do peso.';
+  else if (calculo >= 30 && calculo <= 40)
+    imc.innerHTML = '🍔 🍟 Você está com obesidade.';
+  else
+    imc.innerHTML = '🏥 Você está com obesidade mórbida.'
+}
+
+function verificarInput(event) {
+  var letra = event.key
+
+  if (letra != 0 && letra != 1 && letra != 2 && letra != 3 && letra != 4 && letra != 5 && letra != 6 && letra != 7 && letra != 8 && letra != 9 && letra != ',' && letra != '.' && letra != 'Tab' && letra != 'Enter' && letra != 'Backspace' && letra != 'Delete' && letra != 'ArrowLeft' && letra != 'ArrowRight') {
+    alert("Caractere inválido!")
+    document.getElementById("altura").value = "";
+    document.getElementById("peso").value = "";
+  }
+
+}
